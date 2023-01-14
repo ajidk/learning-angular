da78b0190f7a3b5d231956d003093fd32b6508e3
suraji <surajidk12@gmail.com>
2023-01-14 11:49:28 +0700

    scalable angular structure

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "green-primary": "#66A92D",
+      },
+    },
+  },
+  plugins: [],
+};
